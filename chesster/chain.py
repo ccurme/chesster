c542038ac0e77f2ad780872725380ff737e1f161
Chester Curme <chestercurme@microsoft.com>
2023-12-23 00:01:36 -0500

    support moves and commentary

--- chesster/chain.py
+++ chesster/chain.py
@@ -1,3 +1,5 @@
+from textwrap import dedent
+
 from langchain_community.chat_models import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import Runnable
@@ -9,24 +11,41 @@ MEMORY_KEY = "chat_history"
 
 def get_analysis_chain() -> Runnable:
     """Get Langchain Runnable for analyzing board."""
-    system_message = """You are a seasoned chess instructor.
-    You are witty and sarcastic. You are analyzing a student's game.
-    Help them learn chess and have a good time.
+    system_message = """
+    You are a seasoned chess instructor. You are witty and sarcastic.
+    You are analyzing a student's game. Help them learn chess and have a good time.
 
-    Remember to call out blunders and mistakes.
+    The student will either issue an instruction for a move, or make a query that asks a question
+    or continues the conversation. You will return a response in JSON:
+    {{"move": null or str, "commentary": null or str}}
 
-    Limit your commentary to 20 words or less.
+    If the student issues an instruction for a move, you will infer and provide the legal move
+    in UCI notation. In these cases you can provide commentary, but it is not required. If the
+    student does not issue an instruction for a a move, respond to their query. For example,
+    the student might ask "Can you give me a hint?" or "How could I have defended against that?"
+
+    For example, assuming the student is playing white, these are acceptable responses:
+    "Move my king's pawn up two" --> {{"move": "e2e4", "commentary": "Solid opening."}}
+    "d2d4" --> {{"move": "d2d4", "commentary": null}}
+    "Move my King up one." --> {{"move": "e1e2", "commentary": "Bold choice."}}
+    "Can I have a hint?" --> {{"move": null, "commentary": "You have an opportunity to check here."}}
+
+    Remember to call out blunders and mistakes in your commentary.
+
+    Limit your commentary to 20 words or fewer.
     """
     prompt = ChatPromptTemplate.from_messages(
         [
-            ("system", system_message),
+            ("system", dedent(system_message)),
             MessagesPlaceholder(variable_name=MEMORY_KEY),
             ("system", "{board_context}"),
             ("user", "{user_message}"),
         ]
     )
 
-    llm = ChatOpenAI(model="gpt-4-1106-preview", temperature=0)
+    llm = ChatOpenAI(model="gpt-4-1106-preview", temperature=0).bind(
+        response_format={"type": "json_object"}
+    )
 
     chain = (
         {
