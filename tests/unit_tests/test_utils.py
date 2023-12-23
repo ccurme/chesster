705baf0c43bdd535169399a3ac9ebc6bd6eefa7d
Chester Curme <chestercurme@microsoft.com>
2023-12-23 09:03:42 -0500

    update test

--- tests/unit_tests/test_utils.py
+++ tests/unit_tests/test_utils.py
@@ -13,17 +13,17 @@ def _check_indentation(multi_line_string: str) -> None:
 
 def test_system_message():
     board = chess.Board()
-    player_side = chess.WHITE
-    system_message = utils.make_system_message(board, player_side)
+    board.player_side = chess.WHITE
+    system_message = utils.make_system_message(board)
     _check_indentation(system_message)
     print_text(f"\n{system_message}")
     move = chess.Move.from_uci("e2e4")
     board.push(move)
-    system_message = utils.make_system_message(board, player_side)
+    system_message = utils.make_system_message(board)
     _check_indentation(system_message)
     print_text(f"\n------\n{system_message}")
     move = chess.Move.from_uci("e7e5")
     board.push(move)
-    system_message = utils.make_system_message(board, player_side)
+    system_message = utils.make_system_message(board)
     _check_indentation(system_message)
     print_text(f"\n------\n{system_message}")
