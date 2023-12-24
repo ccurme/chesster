c8e2f0e4de70f7da7900cbea21b1a65fa0280dbe
Chester Curme <chestercurme@microsoft.com>
2023-12-24 13:51:31 -0500

    render chessboards

--- chesster/app.py
+++ chesster/app.py
@@ -40,34 +40,36 @@ class BoardManager:
         self.active_websockets: List[WebSocket] = []
         self.last_updated_image = None
         self.board = chess.Board()
-        self.player_side = None
-        self.global_images = [
-            "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 50 50'%3E%3Crect width='50' height='50' style='fill:rgb(255,0,0);stroke-width:0;stroke:rgb(0,0,0)' /%3E%3C/svg%3E",
-            "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 50 50'%3E%3Crect width='50' height='50' style='fill:rgb(0,0,255);stroke-width:0;stroke:rgb(0,0,0)' /%3E%3C/svg%3E",
-            "data:image/svg+xml," + urllib.parse.quote(str(display_board(self.board, chess.WHITE))),
-        ]
+        self.player_side = chess.WHITE
 
-    async def update_image(self, color: int):
-        if color == 1:
-            self.last_updated_image = self.global_images[0]
-        elif color == 2:
-            self.last_updated_image = self.global_images[1]
-        else:
-            self.last_updated_image = self.global_images[2]
 
+    async def set_player_side(self, player_side: chess.Color) -> None:
+        """Set player side."""
+        self.player_side = player_side
+        await self.update_board(self.board)
+
+
+    async def update_board(self, board: chess.Board) -> None:
+        """Update SVG string."""
+        board_svg = urllib.parse.quote(str(display_board(board, self.player_side)))
+        svg_string = f"data:image/svg+xml,{board_svg}"
+        self.last_updated_image = svg_string
         for websocket in self.active_websockets:
             await websocket.send_text(self.last_updated_image)
+
 
     async def websocket_endpoint(self, websocket: WebSocket):
         await websocket.accept()
         self.active_websockets.append(websocket)
         try:
             while True:
+                await self.update_board(self.board)
                 data = await websocket.receive_text()
                 if data == "Show me the image" and self.last_updated_image is not None:
                     await websocket.send_text(self.last_updated_image)
         except WebSocketDisconnect:
             self.active_websockets.remove(websocket)
+
 
 board_manager = BoardManager()
 
@@ -75,10 +77,24 @@ board_manager = BoardManager()
 async def get():
     return HTMLResponse(html)
 
+
+@app.get("/set_player_side/{color}")
+async def set_player_side(color: str):
+    if "w" in color:
+        player_side = chess.WHITE
+        side_str = "white"
+    else:
+        player_side = chess.BLACK
+        side_str = "black"
+    await board_manager.set_player_side(player_side)
+    return {"message": f"Updated player side successfully to {side_str}."}
+
+
 @app.get("/update_image/{color}")
 async def update_image(color: int):
     await board_manager.update_image(color)
     return {"message": "Image updated successfully", "color": color}
+
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
