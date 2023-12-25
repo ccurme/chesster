e95c1b502184d9f6cee2e472a724705a2175accc
Chester Curme <chestercurme@microsoft.com>
2023-12-25 07:45:20 -0500

    update tests

--- tests/unit_tests/test_agent.py
+++ tests/unit_tests/test_agent.py
@@ -1,13 +1,60 @@
-import chess
+from unittest.mock import MagicMock, patch
+from typing import Callable
 
-from chesster import game_agent
+from langchain_core.messages import AIMessage
+from langchain_core.runnables import Runnable
+
+from chesster import play_stockfish
 
 
-def test_move_tool():
-    """Test move tool."""
-    board = chess.Board()
-    game_agent._make_chess_move(board, "e2e4")
-    assert 1 == len(board.move_stack)
-    board = chess.Board()
-    game_agent._make_chess_move(board, "e4")
-    assert 1 == len(board.move_stack)
+def _make_mock_llm(*args, **kwargs) -> Runnable:
+    """Make mock llm for testing."""
+    llm = MagicMock(spec=Runnable)
+    llm.invoke.side_effect = [
+        AIMessage(
+            content="",
+            additional_kwargs={
+                "function_call": {
+                    "arguments": '{"move":"d2d4"}',
+                    "name": "make_chess_move",
+                }
+            },
+        ),
+        AIMessage(content="Solid opening."),
+        AIMessage(
+            content="",
+            additional_kwargs={
+                "function_call": {
+                    "arguments": '{"move":"b1c3"}',
+                    "name": "make_chess_move",
+                }
+            },
+        ),
+        AIMessage(content=""),
+        AIMessage(
+            content="",
+            additional_kwargs={
+                "function_call": {
+                    "arguments": '{"move":"c1f4"}',
+                    "name": "make_chess_move",
+                }
+            },
+        ),
+        AIMessage(content=""),
+    ]
+    llm.bind = lambda *args, **kwargs: llm
+
+    return llm
+
+
+@patch("chesster.play_stockfish.input", side_effect=["w", "d4", "Nc3", "c1f4"])
+@patch(
+    "chesster.play_stockfish.chess.Board.is_game_over",
+    side_effect=[False, False, False, True],
+)
+@patch("chesster.game_agent.ChatOpenAI", return_value=_make_mock_llm())
+def test_play_stockfish(
+    mock_llm: Callable, mock_is_game_over: Callable, user_input: Callable
+):
+    board = play_stockfish.main()
+    assert 6 == len(board.move_stack)
